7646f2ab3acc94211a3e718d34788cc3a09e5181
OwenLu <gg0125gg@gmail.com>
2023-07-14 15:20:27 +0800

    refactor: folder name adjust

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,10 +2,10 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 // page
-import LoginPage from '../loginPage/loginPage';
-import SignupPage from '../signupPage/signupPage';
-import AdminLoginPage from '../adminLoginPage/adminLoginPage';
-import MainPage from '../mainPage/mainPage';
+import LoginPage from '../LoginPage/LoginPage';
+import SignupPage from '../SignupPage/SignupPage';
+import AdminLoginPage from '../AdminLoginPage/AdminLoginPage';
+import MainPage from '../MainPage/MainPage';
 // style
 import './reset.scss'
 import './base.scss'
