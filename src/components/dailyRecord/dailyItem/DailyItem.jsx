5bed0726520b22a9f861914f55510df7b0963a6d
OwenLu <gg0125gg@gmail.com>
2023-07-20 14:41:27 +0800

    feat: substring date

--- src/components/dailyRecord/dailyItem/DailyItem.jsx
+++ src/components/dailyRecord/dailyItem/DailyItem.jsx
@@ -1,13 +1,20 @@
 import msgIcon from '../../../assets/message.svg';
 import './DailyItem.scss';
 
-export const DailyItem = ({ listNumber, action, created_on, quantity, price }) => {
+export const DailyItem = ({
+  listNumber,
+  action,
+  created_on,
+  quantity,
+  price,
+}) => {
+  const time = created_on.substr(11, 8);
   return (
     <>
       <li>
         <p className='regular-14'>#{listNumber}</p>
         <p className='regular-14'>{action}</p>
-        <p className='regular-14'>12:55</p>
+        <p className='regular-14'>{time}</p>
         <p className='regular-14'>{quantity}</p>
         <p className='regular-14'>{price}</p>
         <img src={msgIcon} alt='message-icon' />
