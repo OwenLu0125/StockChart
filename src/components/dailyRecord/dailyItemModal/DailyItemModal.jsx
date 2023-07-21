55259d6cbfe0afcaf1854d24f79c5a78f190b4d1
OwenLu <gg0125gg@gmail.com>
2023-07-21 16:28:17 +0800

    feat: add basic modal

---
+++ src/components/dailyRecord/dailyItemModal/DailyItemModal.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import msgIcon from '../../../assets/message.svg';
+
+export const DailyItemModal = () => {
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+  return (
+    <>
+      <img src={msgIcon} alt='message-icon' onClick={toggleModal} />
+
+      {modal && (
+        <div className="modal">
+          <div onClick={toggleModal} className="overlay"></div>
+          <div className="modal-content">
+            <h2>Modal</h2>
+            <p>
+              Lorem ipsum dolor 
+            </p>
+            <button className="close-modal" onClick={toggleModal}>
+              CLOSE
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+export default DailyItemModal;
