17cea7684dabff14c16e39ebda82a4011e11ae64
OwenLu <gg0125gg@gmail.com>
2023-07-22 14:55:20 +0800

    feat: add modal to daily item

--- src/components/dailyRecord/dailyItemModal/DailyItemModal.jsx
+++ src/components/dailyRecord/dailyItemModal/DailyItemModal.jsx
@@ -13,16 +13,28 @@ export const DailyItemModal = () => {
       <img src={msgIcon} alt='message-icon' onClick={toggleModal} />
 
       {modal && (
-        <div className="modal">
-          <div className="overlay"></div>
-          <div className="modal-content">
-            <h2>Modal</h2>
-            <p>
-              Lorem ipsum dolor 
-            </p>
-            <button className="close-modal" onClick={toggleModal}>
-              CLOSE
-            </button>
+        <div className='modal'>
+          <div className='overlay'></div>
+          <div className='modal-content'>
+            <h2>
+              <img src={msgIcon} alt='' />
+              備註
+            </h2>
+            <div className='textSection'>
+              <textarea
+                className='modalInput'
+                type='text'
+                placeholder='為您儲存的交易做些筆記吧？'
+              />
+            </div>
+            <div className='btnSection'>
+              <button
+                className='secondary-button bold-16'
+                onClick={toggleModal}>
+                取消
+              </button>
+              <button className='primary-button bold-16'>確定</button>
+            </div>
           </div>
         </div>
       )}
