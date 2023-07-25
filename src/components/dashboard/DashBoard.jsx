a4aa7375c413c4eab7d51b8e424dd9342b4c6e1f
OwenLu <gg0125gg@gmail.com>
2023-07-25 11:43:23 +0800

    feat: pass data to component

--- src/components/dashboard/DashBoard.jsx
+++ src/components/dashboard/DashBoard.jsx
@@ -7,20 +7,31 @@ export const DashBoard = ({
   winRate,
   totalWinPoints,
   totalLossPoints,
-  pAndL,
-  roundTrip,
+  pAndL, // 總損益
+  roundTrip, // 總趟次
   netPAndL,
-  riskRatio,
+  riskRatio, // 盈虧比
 }) => {
-
   return (
     <>
       <div className='dashBoardContainer'>
-        <TopIncome />
+        <TopIncome
+          pAndL={pAndL}
+          riskRatio={riskRatio}
+          averageLossPoints={averageLossPoints}
+          averageWinPoints={averageWinPoints}
+          roundTrip={roundTrip}
+        />
         <div className='chartSection'>
           <div className='leftChart'>
-            <div className='chart'>勝率%/交易量</div>
-            <div className='chart'>勝率%/天數</div>
+            <div className='chart'>
+              <p>勝率：{winRate ?? "test"}</p>
+              <p>盈餘次數：{totalWinPoints ?? "test"}</p>
+              <p>盈虧次數：{totalLossPoints ?? "test"}</p>
+            </div>
+            <div className='chart'>
+              <p>勝率%/天數</p>
+            </div>
           </div>
           <div className='rightChart'>這是右邊的圖表區域</div>
         </div>
--- src/components/dashboard/topIncome/TopIncome.jsx
+++ src/components/dashboard/topIncome/TopIncome.jsx
@@ -1,27 +1,33 @@
 import React from 'react';
 import './TopIncome.scss';
 
-export const TopIncome = () => {
+export const TopIncome = ({
+  averageLossPoints,
+  averageWinPoints,
+  pAndL,// 總損益
+  roundTrip, // 總趟次
+  riskRatio, // 盈虧比
+}) => {
   return (
     <>
       <div className='topIncomeContainer'>
         <div className='toatalIncome'>
           <div>整體總損益</div>
-          <div>$1,400,000</div>
-          <div>總交易量： 127</div>
+          <div>{pAndL}</div>
+          <div>總交易量： {roundTrip}</div>
         </div>
         <div className='averageIncome'>
           <div className='average1'>
             <div>獲利係數</div>
-            <div>3.19</div>
+            <div>{riskRatio ?? 'test'}</div>
           </div>
           <div className='average2'>
             <div>平均盈餘</div>
-            <div>$439,082</div>
+            <div>{averageLossPoints ?? 'test'}</div>
           </div>
           <div className='average3'>
             <div>平均盈虧</div>
-            <div>$393,200</div>
+            <div>{averageWinPoints ?? 'test'}</div>
           </div>
         </div>
       </div>
