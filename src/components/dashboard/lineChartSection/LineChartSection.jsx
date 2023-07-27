c422826ed84b1a63a730f24ad9d9656b3c282920
OwenLu <gg0125gg@gmail.com>
2023-07-27 15:33:46 +0800

    feat: draw line chart

--- src/components/dashboard/lineChartSection/LineChartSection.jsx
+++ src/components/dashboard/lineChartSection/LineChartSection.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import './LineChartSection.scss';
 import {
   LineChart,
@@ -9,55 +10,10 @@ import {
   Legend,
 } from 'recharts';
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
-
 const CustomizedDot = (props) => {
   const { cx, cy, value } = props;
 
-  if (value > 2500) {
+  if (value > 80) {
     return (
       <svg
         x={cx - 10}
@@ -84,12 +40,22 @@ const CustomizedDot = (props) => {
   );
 };
 
-export const LineChartSection = () => {
+export const LineChartSection = ({ transactions }) => {
+  const [newData, setNewData] = useState([]);
+  console.log(transactions);
+  useEffect(() => {
+    const temData = transactions.map((item) => ({
+      date: item.transaction_date.slice(5, 10),
+      pandl: item.pandl ?? 0,
+    }));
+    setNewData(temData);
+  }, [transactions ]);
+  console.log(newData);
   return (
     <LineChart
-      width={500}
+      width={600}
       height={400}
-      data={data}
+      data={newData}
       margin={{
         top: 5,
         right: 30,
@@ -97,17 +63,16 @@ export const LineChartSection = () => {
         bottom: 5,
       }}>
       <CartesianGrid strokeDasharray='3 3' />
-      <XAxis dataKey='name' />
+      <XAxis dataKey='date' />
       <YAxis />
       <Tooltip />
       <Legend />
       <Line
         type='monotone'
-        dataKey='pv'
+        dataKey='pandl'
         stroke='#8884d8'
         dot={<CustomizedDot />}
       />
-      <Line type='monotone' dataKey='uv' stroke='#82ca9d' />
     </LineChart>
   );
 };
