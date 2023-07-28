077c49738cf137f0197140a209e4b016f780c01e
kaihsu12 <kaihsu12@gmail.com>
2023-07-28 13:49:40 +0800

    add new font for scss base and new route

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,7 @@
 // react
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-// page
+// pages
 import LoginPage from '../loginPage/LoginPage';
 import SignupPage from '../signupPage/SignupPage';
 import AdminLoginPage from '../adminLoginPage/AdminLoginPage';
@@ -9,6 +9,9 @@ import MainPage from '../mainPage/MainPage';
 import DiaryPage from '../diaryPage/DiaryPage';
 import HistoryPage from '../historyPage/HistoryPage';
 import SettingPage from '../settingPage/SettingPage';
+import ReplyPage from '../replyPage/ReplyPage';
+// context
+import { IdProvider } from '../../contexts/IdContext';
 // style
 import './reset.scss';
 import './base.scss';
@@ -18,15 +21,18 @@ const Home = () => {
     <div>
       <BrowserRouter>
         {/* <AuthProvider> */}
-        <Routes>
-          <Route path='*' element={<LoginPage />} />
-          <Route path='signup' element={<SignupPage />} />
-          <Route path='admin' element={<AdminLoginPage />} />
-          <Route path='main' element={<MainPage />} />
-          <Route path='diary' element={<DiaryPage />} />
-          <Route path='history' element={<HistoryPage />} />
-          <Route path='setting' element={<SettingPage />} />
-        </Routes>
+        <IdProvider>
+          <Routes>
+            <Route path='*' element={<LoginPage />} />
+            <Route path='signup' element={<SignupPage />} />
+            <Route path='admin' element={<AdminLoginPage />} />
+            <Route path='main' element={<MainPage />} />
+            <Route path='diary' element={<DiaryPage />} />
+            <Route path='history' element={<HistoryPage />} />
+            <Route path='reply' element={<ReplyPage />} />
+            <Route path='setting' element={<SettingPage />} />
+          </Routes>
+        </IdProvider>
         {/* </AuthProvider> */}
       </BrowserRouter>
     </div>
