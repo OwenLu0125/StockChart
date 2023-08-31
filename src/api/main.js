d7d148b02ecc1a3688f4a786ec89c9ffae11132d
kaihsu12 <kaihsu12@gmail.com>
2023-08-31 15:19:56 +0800

    add api to fetch data of user

--- src/api/main.js
+++ src/api/main.js
@@ -10,3 +10,14 @@ export const getRanking = async () => {
     console.error('[Get Ranking Data failed]: ', error);
   }
 };
+
+export const getCurrentUser = async () => {
+  try {
+    const data = await axiosInstance.get(`${baseUrl}/users`, {
+      withCredentials: true,
+    });
+    return data;
+  } catch (error) {
+    console.error('[Get User Data failed]: ', error);
+  }
+};
