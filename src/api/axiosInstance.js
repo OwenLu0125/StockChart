8646c4a06a690e72ece46e5cf7d67f309bf80b33
OwenLu <gg0125gg@gmail.com>
2023-08-31 17:39:17 +0800

    feat: accessToken&refreshToken

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { decodeToken } from 'react-jwt';
+import dayjs from 'dayjs';
 
 export const baseUrl = 'https://trade-tracker.onrender.com/api';
 
@@ -6,18 +8,33 @@ const axiosInstance = axios.create({
   baseURL: baseUrl,
 });
 
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('authToken');
+axiosInstance.interceptors.request.use(async (config) => {
+  const authToken = localStorage.getItem('authToken')
+    ? JSON.parse(localStorage.getItem('authToken'))
+    : null;
+  console.log(authToken);
+  if (authToken)
+    config.headers['Authorization'] = `Bearer ${authToken.accessToken}`;
 
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
+  const user = decodeToken(authToken.accessToken);
+  const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1;
+  console.log('isExpired', isExpired);
+
+  if (!isExpired) return config;
+  try {
+    const res = await axios.post(`${baseUrl}/users/refreshToken`, {
+      refreshToken: authToken.refreshToken,
+    });
+    console.log(res);
+    authToken.accessToken = res.data.accessToken;
+    console.log(authToken);
+    localStorage.setItem('authToken', JSON.stringify(authToken));
+    config.headers['Authorization'] = `Bearer ${authToken.accessToken}`;
+  } catch (error) {
     console.error(error);
   }
-);
+
+  return config;
+});
 
 export default axiosInstance;
