86cff340676d5600f332fc6f6b3b48c675f0b2d9
kaihsu12 <kaihsu12@gmail.com>
2023-09-02 14:26:32 +0800

    remove func for fetching api

--- src/api/main.js
+++ src/api/main.js
@@ -8,33 +8,3 @@ export const getRanking = async () => {
     console.error('[Get Ranking Data failed]: ', error);
   }
 };
-
-export const getCurrentUser = async () => {
-  try {
-    const { data } = await axiosInstance.get(`${baseUrl}/users`);
-
-    return data.user;
-  } catch (error) {
-    console.error('[Get User Data failed]: ', error);
-  }
-};
-
-export const refreshToken = async () => {
-  const token = JSON.parse(localStorage.getItem('authToken'));
-  try {
-    if (token) {
-      const res = await axiosInstance.post(`${baseUrl}/users/refreshToken`, {
-        refreshToken: token.refreshToken,
-      });
-      console.log('token更新成功', res);
-      return res;
-    } else {
-      const res = await axiosInstance.post(`${baseUrl}/users/refreshToken`);
-      console.log('token更新成功', res);
-      return res;
-    }
-  } catch (error) {
-    console.error('Get Token failed !!!', error);
-    return error;
-  }
-};
